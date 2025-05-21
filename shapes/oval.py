56fa747fc2bf75d7914425be2afb818fe7c89dd0
noam-kimhi <noamkimhi123@gmail.com>
2025-05-21 23:06:56 +0300

    Update oval.py

--- shapes/oval.py
+++ shapes/oval.py
@@ -1,6 +1,6 @@
 #################################################################
 # FILE : oval.py
-# WRITER : Noam Kimhi, noam.kimhi, ID: 322678947
+# WRITER : Noam Kimhi, noam.kimhi
 # EXERCISE : intro2cs Final Project
 # DESCRIPTION: Paint Program - Oval Class
 #################################################################
