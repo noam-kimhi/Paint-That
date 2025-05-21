a319c5329f33a7bf8c272f6fe3b9cb66e6609198
noam-kimhi <noamkimhi123@gmail.com>
2025-05-21 23:07:04 +0300

    Update polygon.py

--- shapes/polygon.py
+++ shapes/polygon.py
@@ -1,6 +1,6 @@
 #################################################################
 # FILE : polygon.py
-# WRITER : Noam Kimhi, noam.kimhi, ID: 322678947
+# WRITER : Noam Kimhi, noam.kimhi
 # EXERCISE : intro2cs Final Project
 # DESCRIPTION: Paint Program - Polygon Class
 #################################################################
