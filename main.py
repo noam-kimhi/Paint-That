f5f120f881b1641adf7121ada49f46a2b9e687bd
noam-kimhi <noamkimhi123@gmail.com>
2025-05-21 23:08:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #################################################################
 # FILE : main.py
-# WRITER : Noam Kimhi, noam.kimhi, ID: 322678947
+# WRITER : Noam Kimhi, noam.kimhi
 # EXERCISE : intro2cs Final Project
 # DESCRIPTION: Paint Program - Paint That - Main file
 #################################################################
