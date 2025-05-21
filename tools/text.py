c2ca54e4a8fa63c5dfd234daf738229ead0b5872
noam-kimhi <noamkimhi123@gmail.com>
2025-05-21 23:05:53 +0300

    Update and rename text.py to tools/text.py

--- tools/text.py
+++ tools/text.py
@@ -1,6 +1,6 @@
 #################################################################
 # FILE : text.py
-# WRITER : Noam Kimhi, noam.kimhi, ID: 322678947
+# WRITER : Noam Kimhi, noam.kimhi
 # EXERCISE : intro2cs Final Project
 # DESCRIPTION: Paint Program - Text Class
 #################################################################
