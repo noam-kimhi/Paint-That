2568fb585f3ac45c0dd272a9b4b1abc402556c55
noam-kimhi <noamkimhi123@gmail.com>
2025-05-21 23:04:03 +0300

    Update and rename pixel_pen.py to pixel_art/pixel_pen.py

--- pixel_art/pixel_pen.py
+++ pixel_art/pixel_pen.py
@@ -1,6 +1,6 @@
 #################################################################
 # FILE : pixel_pen.py
-# WRITER : Noam Kimhi, noam.kimhi, ID: 322678947
+# WRITER : Noam Kimhi, noam.kimhi
 # EXERCISE : intro2cs Final Project
 # DESCRIPTION: Paint Program - PixelPen Class
 #################################################################
